f090d93fd1b1cb06279b8ea3c09c74e8aa8fbf60
rabbitbaka <95219232+rabbitbaka@users.noreply.github.com>
2024-07-25 12:22:26 +0900

    객체 중복 생성 문제 해결

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -12,36 +12,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class MemoController {
 
-    //복사
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoService memoService;
 
     public MemoController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoService = new MemoService(jdbcTemplate);
     }
 
     //PostMappin 변경
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate); //인스턴스화
         return memoService.createMemo(requestDto); //컨트롤러와 서비스의 매소드를 똑같이 하기(협업할 때를 위해)
 
     }
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.deleteMemo(id);
     }
 
